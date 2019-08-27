7e87f1cd381a1d60276142293ee1955116a9c1a3
Joanne <joannesayswu@gmail.com>
2019-08-27 13:14:36 -0700

    add first pass at solving is palindrome by converting input to string

--- remove-element.js
+++ remove-element.js
@@ -1,5 +1,7 @@
 // source: https://leetcode.com/problems/remove-element/
 
+// solutions: https://leetcode.com/articles/remove-element/
+
 // Given an array nums and a value val, remove all instances of that value in-place and return the new length.
 
 // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
