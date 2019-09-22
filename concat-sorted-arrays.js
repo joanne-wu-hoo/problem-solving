e821de64be45cbab30b1123eaa288159d373b55c
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2019-09-21 20:09:30 -0700

    Update concat-sorted-arrays.js

--- concat-sorted-arrays.js
+++ concat-sorted-arrays.js
@@ -7,32 +7,31 @@ all the values from both arrays without mutating the input arrays */
 function concatSortedArrays(nums1, nums2){
   let i = 0; // nums1 pointer
   let j = 0; // nums2 pointer
-  let sorted = [];
+  let result = [];
 
   while (i < nums1.length && j < nums2.length){
     if (nums1[i] <= nums2[j]){
-      sorted.push(nums1[i]);
+      result.push(nums1[i]);
       i++;
     } else if (nums1[i] > nums2[j]){
-      sorted.push(nums2[j]);
+      result.push(nums2[j]);
       j++;      
     }
   }
 
-  // if we have not depleted nums1, add the remaining values
-  if (i <= nums1.length-1){
-    for (let k = i; k < nums1.length; i++){
-      sorted.push(nums1[k]);
-    }
+  // if arr1 was longer, push the rest of arr1
+  while (i < nums1.length) {
+    result.push(nums1[i]);
+    i++;
   }
-  // if we have not depleted nums2, add the remaining values
-  if (j <= nums2.length-1){
-    for (let k = j; k < nums2.length; k++){
-      sorted.push(nums2[k]);
-    }
-  } 
 
-  return sorted;
+  // if arr2 was longer, push the rest of arr2
+  while (j < nums2.length) {
+    result.push(nums2[j]);
+    j++;
+  }
+
+  return result;
 }
 
 console.log(concatSortedArrays([1, 3, 4, 5], [2, 4, 6, 8])) // [1, 2, 3, 4, 4, 5, 6, 8]);
