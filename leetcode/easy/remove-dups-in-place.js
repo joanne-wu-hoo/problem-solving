2814ca296d5e71c036379a81f9df3fd0d3266224
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-05-16 17:48:49 -0700

    Update remove-dups-in-place.js

--- leetcode/easy/remove-dups-in-place.js
+++ leetcode/easy/remove-dups-in-place.js
@@ -19,3 +19,30 @@ function removeDupInPlace(nums){
 }
 
 removeDupInPlace([0,0,1,1,1,2,2,3,3,4]); // 5
+
+
+// Using less space
+function removeDuplicates(nums) {
+    if (nums.length <= 1){
+        return nums.length;
+    };
+    
+    let prevVal = nums[0];
+    let validNumLength = 1;
+    
+    for (let i = 1; i < nums.length; i++){
+        let currentVal = nums[i];
+        
+        if (currentVal !== prevVal){
+            // found a new valid number
+            validNumLength++;
+            prevVal = nums[i];
+            // move valid number to end of valid number section
+            [nums[validNumLength-1]] = [nums[i]];
+        }
+    }
+    
+    
+    return validNumLength;
+}
+
