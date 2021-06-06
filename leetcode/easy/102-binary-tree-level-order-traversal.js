73cb55762273ef08581d9dbc285e1368a522ed94
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-05 18:15:46 -0700

    Create 102-binary-tree-level-order-traversal.js

---
+++ leetcode/easy/102-binary-tree-level-order-traversal.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+// BFS - queue - push/shift
+
+// Values will be the values that are returned by BFS, but we need a way to determine when we're at a new level
+
+// When we're at the root, we have 1 node to visit -- so our numNodesToVisit = 1
+// While numNodesToVisit > 0:
+// - Add values to our currentLevel [] 
+// - Keep on adding to our toVisit stack as normal
+// When numNodesToVisit = 0, we've finished a level:
+// - numNodesToVisit for the next level is now the length of the toVisit stack
+// - Add currentLevel[] to output[]
+
+var levelOrder = function(root) {
+    if (!root) return [];
+    
+    let toVisit = [root];
+    let output = [];
+    let numNodesToVisit = 1;
+    
+    while (toVisit.length){
+        let currentLevel = [];
+        
+        while (numNodesToVisit){
+            let currentNode = toVisit.shift();
+            currentLevel.push(currentNode.val);
+            if (currentNode.left) toVisit.push(currentNode.left)
+            if (currentNode.right) toVisit.push(currentNode.right)
+            numNodesToVisit--;
+        }
+        
+        numNodesToVisit = toVisit.length;
+        output.push(currentLevel);
+    }  
+    
+    return output;
+}
