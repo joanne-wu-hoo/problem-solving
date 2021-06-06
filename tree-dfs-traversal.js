e3230f838f505b5db4d86be7f17a20c010fe974a
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-05 19:29:54 -0700

    Create tree-dfs-traversal.js

---
+++ tree-dfs-traversal.js
@@ -0,0 +1,25 @@
+// DFS - stack - LIFO push/pop
+
+// In order
+function traverseInOrderDFS = (node = root, data=[]) => {
+  if (node.left) traverseInOrderDFS(node.left);
+  data.push(node.val);
+  if (node.right) traverseInOrderDFS(node.right);
+  return data;
+}
+
+// Pre order
+function traversePreOrderDFS = (node = root, data=[]) => {
+  data.push(node.val);
+  if (node.left) traverseInOrderDFS(node.left);
+  if (node.right) traverseInOrderDFS(node.right);
+  return data;
+}
+
+// Post order 
+function traversePostOrderDFS = (node = root, data=[]) => {
+  if (node.left) traverseInOrderDFS(node.left);
+  if (node.right) traverseInOrderDFS(node.right);
+  data.push(node.val);
+  return data;
+}
