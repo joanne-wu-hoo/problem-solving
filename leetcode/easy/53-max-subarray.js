93b155f7ffb24727c52cb2866b117e044392ed4a
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-06 17:00:58 -0700

    Update 53-max-subarray.js

--- leetcode/easy/53-max-subarray.js
+++ leetcode/easy/53-max-subarray.js
@@ -1,14 +1,33 @@
 // https://leetcode.com/problems/maximum-subarray/
 
-var maxSubArray = function(nums) {
-    if (nums.length === 1) return nums[0]
+function maxSubArray(nums){
+  let sum = nums[0];
+  let maxSum = nums[0];
 
-    let maxSum = nums[0] + nums[1];
-    let currentSum = maxSum;
-        
-    for (let i = 2; i < nums.length; i++){
-        currentSum = currentSum + nums[i] - nums[i-1];
-        maxSum = Math.max(currentSum, maxSum)
+  for (let right = 1; right < nums.length; right++){
+    // is the sum larger if we add the right value OR if we start at the right value?
+    if (nums[right] > sum + nums[right]){
+      // if sum is larger if we start at the right value, set sum as nums[right]
+      sum = nums[right];
+    } else {
+      // else if sum is larger with the right value, set sum as sum += nums[right]
+      sum += nums[right];
+    }
+    
+    maxSum = Math.max(maxSum, sum)
+  }
+
+  return maxSum;
+}
+
+// The above can be refactored into
+function maxSubArray(nums) {
+    let maxSum = nums[0];
+    let currentMaxSum = nums[0];
+    
+    for (let i = 1; i < nums.length; i ++){
+        currentMaxSum = Math.max(nums[i], nums[i] + currentMaxSum);
+        maxSum = Math.max(currentMaxSum, maxSum);
     }
     
     return maxSum;
