308f402041d2b64f3ed761f272e98b1a7f883c43
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-15 20:40:56 -0700

    Update tree-dfs-traversal.js

--- tree-dfs-traversal.js
+++ tree-dfs-traversal.js
@@ -2,24 +2,33 @@
 
 // In order
 function traverseInOrderDFS = (node = root, data=[]) => {
+  if (!node) return [];
+  
   if (node.left) traverseInOrderDFS(node.left);
   data.push(node.val);
   if (node.right) traverseInOrderDFS(node.right);
+  
   return data;
 }
 
 // Pre order
 function traversePreOrderDFS = (node = root, data=[]) => {
+  if (!node) return [];
+  
   data.push(node.val);
   if (node.left) traverseInOrderDFS(node.left);
   if (node.right) traverseInOrderDFS(node.right);
+  
   return data;
 }
 
 // Post order 
 function traversePostOrderDFS = (node = root, data=[]) => {
+  if (!node) return [];
+  
   if (node.left) traverseInOrderDFS(node.left);
   if (node.right) traverseInOrderDFS(node.right);
   data.push(node.val);
+  
   return data;
 }
