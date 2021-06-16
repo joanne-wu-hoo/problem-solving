4b9a45064fd30e972c93e64db1d4d4a1a2b03769
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-15 18:46:43 -0700

    Update 78-subsets.js

--- leetcode/easy/78-subsets.js
+++ leetcode/easy/78-subsets.js
@@ -19,8 +19,30 @@ function subsets(nums){
 // Image explanining approach
 // https://user-images.githubusercontent.com/9150788/76787122-523f3300-67b0-11ea-8817-e70df9d99987.png
 
+// turning the iterative solution above into a recursive solution
+function subsets(nums) {
+    output = [[]];
+    
+    subsetsHelper(nums,0,output);
+    
+    return output;
+}
 
-// recursive way
+function subsetsHelper(nums, idx, output) {
+    if (idx == nums.length) {
+        return
+    }
+    
+    let num_els = output.length
+    for (let i = 0; i < num_els; ++i) {
+       output.push(output[i].concat(nums[idx])) 
+    }
+    
+    subsetsHelper(nums, idx+1, output)
+}
+
+
+// recursive way from leetcode person
 var subsets = function(nums) {
     let result = [];
     
