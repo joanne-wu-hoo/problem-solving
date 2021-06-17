fe5fbf0a838294cee4b7b5dae31c5cd93ffccbe7
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-16 18:06:09 -0700

    Create 238-product-of-array-except-self.js

---
+++ leetcode/easy/238-product-of-array-except-self.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/product-of-array-except-self/
+
+var productExceptSelf = function(nums) {
+    let prodOfValsToLeft = [1];
+    let prod = 1;
+    for (let i = 0; i < nums.length-1; i++){
+        prod = prod * nums[i];
+        prodOfValsToLeft.push(prod);
+    }
+        
+    let prodOfValsToRight = [1];
+    prod = 1;
+    for (let i = nums.length-1; i > 0; i--){
+        prod = prod * nums[i];
+        prodOfValsToRight.unshift(prod);
+    }
+    
+    let products = [];
+    for (let i = 0; i < nums.length; i++){
+        products.push(prodOfValsToLeft[i] * prodOfValsToRight[i]);
+    }
+    
+    return products;
+};
