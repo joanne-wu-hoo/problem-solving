ef25b85cc62d5e7e8ebbccba42749f2ba29a0863
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-20 21:54:17 -0700

    Create count-subarrays-that-sum-to-target.js

---
+++ leetcode/easy/count-subarrays-that-sum-to-target.js
@@ -0,0 +1,28 @@
+var subarraySum = function(nums, k) {
+    nums = nums.sort((a,b) => a-b);
+    
+    let count = 0;
+    let sum = nums[0];
+    let left = 0;
+    let right = 0;
+
+    // sum is inclusive of left and right
+    
+    while (right < nums.length){
+        // if sum === k, count++, left++ (sum - val[left])
+        if (sum === k){
+            count++;
+            sum = sum - nums[left];
+            left++;
+        } else if (sum < k){
+            right++;
+            sum = sum + nums[right];
+        } else { // sum > k
+            sum = sum - nums[left];
+            left++;
+        }
+        
+    }
+
+    return count;
+};
