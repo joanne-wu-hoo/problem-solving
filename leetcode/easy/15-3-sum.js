169e08bc1da01e3f36796e98768cda3f4104cfb5
joanne-wu-hoo <38114845+joanne-wu-hoo@users.noreply.github.com>
2021-06-25 20:41:07 -0700

    Create 15-3-sum.js

---
+++ leetcode/easy/15-3-sum.js
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/3sum/
+
+// If you fix one of the numbers, the problem becomes 2sum!
+
+function threeSum(nums) {
+  if (nums.length < 3) return [];
+
+  nums = nums.sort((a, b) => a - b);
+  let output = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log("in three sum evaluating: ", nums[i]);
+    // if i is above the target, we don't need to continue looking (since the array is sorted)
+    if (nums[i] > 0) return output;
+
+    // if the number is a duplicate, we also dont need to evaluate it
+    if (nums[i] === nums[i - 1]) continue;
+
+    // fix "num" as nums[i]
+    let ntf = 0 - nums[i];
+    let pairs = twoSumSorted(nums.slice(i + 1), ntf);
+
+    for (let pair of pairs) {
+      output.push([nums[i]].concat(pair));
+    }
+  }
+
+  return output;
+}
+
+function twoSumSorted(nums, target) {
+  let output = [];
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+
+    if (sum === target) {
+      output.push([nums[left], nums[right]]);
+      left++;
+    } else if (sum < target) {
+      // go right
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return output;
+}
+
+// twoSumSorted([-1, 0, 1, 2], 1); // [-1,2],[0,1]
+
+threeSum([-1, 0, 1, 2, -1, -4]); // [[-1,-1,2],[-1,0,1]]
